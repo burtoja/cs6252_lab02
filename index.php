12e64c2c632cf1f5a9e148b80ece9bc8316d82d4
J. Allen Burton <jburton6@my.westga.edu>
2020-01-23 22:37:57 -0500

    Built edit product form and created button in product table to take user to edit product page.

--- index.php
+++ index.php
@@ -77,6 +77,7 @@ $statement3->closeCursor();
                 <th>Name</th>
                 <th class="right">Price</th>
                 <th>&nbsp;</th>
+                <th>&nbsp;</th>
             </tr>
 
             <?php foreach ($products as $product) : ?>
@@ -84,13 +85,24 @@ $statement3->closeCursor();
                 <td><?php echo $product['productCode']; ?></td>
                 <td><?php echo $product['productName']; ?></td>
                 <td class="right"><?php echo $product['listPrice']; ?></td>
-                <td><form action="delete_product.php" method="post">
-                    <input type="hidden" name="product_id"
-                           value="<?php echo $product['productID']; ?>">
-                    <input type="hidden" name="category_id"
-                           value="<?php echo $product['categoryID']; ?>">
-                    <input type="submit" value="Delete">
-                </form></td>
+                <td>
+	                <form action="delete_product.php" method="post">
+	                    <input type="hidden" name="product_id"
+	                           value="<?php echo $product['productID']; ?>">
+	                    <input type="hidden" name="category_id"
+	                           value="<?php echo $product['categoryID']; ?>">
+	                    <input type="submit" value="Delete">
+	                </form>
+                </td>
+                <td>
+	                <form action="edit_product_form.php" method="post">
+	                    <input type="hidden" name="product_id"
+	                           value="<?php echo $product['productID']; ?>">
+	                    <input type="hidden" name="category_id"
+	                           value="<?php echo $product['categoryID']; ?>">
+	                    <input type="submit" value="Edit">
+	                </form>
+                </td>
             </tr>
             <?php endforeach; ?>
         </table>
